7450b0a1674add49fe303c08773b39100ceb83c4
Andypanda2k19 <79279184+Andypanda2k19@users.noreply.github.com>
2025-06-11 13:54:37 +0300

    Update script2.py

--- script2.py
+++ script2.py
@@ -124,7 +124,7 @@ def setup_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-application-cache")
     chrome_options.add_argument("--disk-cache-size=0")
-    chrome_options.add_argument(f"--user-data-dir=/tmp/chrome_profile_{random.randint(1,10000)}")
+    chrome_options.add_argument(f"--user-data-dir=/tmp/chrome-data-{uuid.uuid4()}")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument(
